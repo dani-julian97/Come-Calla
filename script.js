ada29d765f834fa0105ea536f20ccac966392280
djulian <djulian@seqeuntiabiotech.com>
2026-01-04 23:28:57 +0100

    changes

--- script.js
+++ script.js
@@ -284,11 +284,11 @@ function setupEventListeners() {
     });
     
     // Cerrar modales
-    document.querySelector('.close').addEventListener('click', () => {
+    document.querySelector('.back-btn:not(.back-detail)').addEventListener('click', () => {
         document.getElementById('recipeModal').style.display = 'none';
     });
     
-    document.querySelector('.close-detail').addEventListener('click', () => {
+    document.querySelector('.back-detail').addEventListener('click', () => {
         document.getElementById('recipeDetailModal').style.display = 'none';
     });
     
